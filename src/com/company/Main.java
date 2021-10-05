6b1e2feb5ca2a6af7fbb07a4901d9d0ed5e8a07e
akhmadferoz <88190058+akhmadferoz-maqsad@users.noreply.github.com>
2021-10-05 19:23:02 +0500

    [FEAT] Completed implementation described in Q1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,38 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        int val, sum = 0;
+
+        Scanner scan = new Scanner(System.in);
+
+        String line;
+
+        System.out.println("Enter a line of text");
+
+        Scanner scanLine = new Scanner(scan.nextLine());
+
+        while (scanLine.hasNext()) {
+            try {
+                val = Integer.parseInt(scanLine.next());
+                sum += val;
+            } catch (Exception e) {
+//                System.out.println("Exception: " + e);
+            }
+        }
+
+//        try {
+//            while (scanLine.hasNext()) {
+//                val = Integer.parseInt(scanLine.next());
+//                sum += val;
+//            }
+//        } catch (Exception e) {
+//        }
+
+        System.out.println("The sum of the integers on this line is " + sum);
     }
 }
